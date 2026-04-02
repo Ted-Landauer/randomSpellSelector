e4144c89b58b5c0b07db70749ffd818cfed3a057
Ted Landauer <tedlandauer@gmail.com>
2026-04-01 17:44:28 -0700

    adding in changes to autopopulate a text area

--- randomSpellSelector.js
+++ randomSpellSelector.js
@@ -1,26 +1,102 @@
-//import data from "./TestSpells.json" assert { type: "json"}
+
 
 console.log("Hello World!");
 
 
 
-//const temp = require('./TestSpells.json');
 
-//console.log(data)
+const testData = {
+	"druid":[
+		{
+			"first": [
+				"absorb elements",
+				"ambush",
+				"bloodhound",
+				"detect magic",
+				"entangle",
+			],
+			"second": [
+				"darkvision",
+				"desecrate",
+				"earthbind",
+				"enhance ability",
+				"find traps",
+			],
+			"third": [
+				"dispel magic",
+				"daylight",
+				"feign death",
+				"poison",
+				"wind wall",
+			]
+		},
+	],
+	"cleric":[
+		{
+			"first": [
+				"bane",
+				"bless",
+				"brimstone",
+				"command",
+				"cure wounds",
+			],
+			"second": [
+				"aid",
+				"augury",
+				"egg",
+				"enhance ability",
+				"locate object",
+			],
+			"third": [
+				"animate dead",
+				"daylight",
+				"feign death",
+				"iron mind",
+				"revivify",
+			]
+		},
+	],
+};
 
 
-async function loadData() {
+
+
+
+
+//const jsonString = JSON.stringify(testData);
+
+
+//test for accessing data in specific json
+//OBJECT["CLASS VALUE"][INDEX(note, there is only 1 index in this format)].NAMED LIST[POSITION IN NAMED LIST]
+const tempVal = testData["druid"][0].second[2]
+
+const tempVal2 = testData["druid"][0].first
+
+
+console.log("printing temp val")
+console.log(tempVal)
+
+console.log("printing temp val 2")
+console.log(tempVal2)
+
+
+function populateList (activeClass) {
+	spellList = testData[activeClass][0];
 	
-		const requestURL = "https://github.com/Ted-Landauer/randomSpellSelector/blob/main/TestSpells.json";
-		const request = new Request(requestURL);
+	fullList = "";
+	
+	for (const level in spellList) {
 		
-		const response = await fetch(request);
-		
-		
-		const spells = await response.json();
-		
-		console.log(spells);
+		for (const spell in spellList[level]) {
+			
+			fullList += spellList[level][spell] + "\n";
+			
+			
+		}
+	}
+	
+	document.getElementById("availableSpells").innerHTML = fullList;
 	
 }
-loadData();
+
 
