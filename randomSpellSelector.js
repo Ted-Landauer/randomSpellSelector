b2036f43e34a454bd4524dc215b381ae29d95929
Ted Landauer <tedlandauer@gmail.com>
2026-04-02 18:12:54 -0700

    adding in the days work so far: - added helper functions for data gathering - added logic to generate a subset list - added logic to randomly pull from lists in a true random and structured random way - making an attempt at reading in the json files needed for live site testing

--- randomSpellSelector.js
+++ randomSpellSelector.js
@@ -1,10 +1,3 @@
-
-
-console.log("Hello World!");
-
-
-
-
 const testData = {
 	"druid":[
 		{
@@ -58,10 +51,27 @@ const testData = {
 	],
 };
 
+let classList = ""
+let spellLevel = 0
+let totalSpells = 0
+let randomType = "structured"
 
+let first = []
+let second = []
+let third = []
+let fourth = []
+let fifth = []
+let sixth = []
+let seventh = []
+let eighth = []
+let ninth = []
 
-
-
+fetch("./TestSpells.json")
+	.then(response => response.json())
+	.then(data => {
+		console.log(data);
+	})
+	.catch(error => console.error("error loading JSON:" error));
 
 //const jsonString = JSON.stringify(testData);
 
@@ -80,23 +90,666 @@ console.log("printing temp val 2")
 console.log(tempVal2)
 
 
-function populateList (activeClass) {
-	spellList = testData[activeClass][0];
+
+document.addEventListener("DOMContentLoaded", function() {
+	const classElement = document.getElementById("selectList");
+	classElement.addEventListener("change", function() {
+		classList = classElement.value;
+		console.log("value changed");
+		console.log(classElement.value)
+	});
+	
+	const spellLevelElement = document.getElementById("spellLevelLimit");
+	spellLevelElement.addEventListener("change", function() {
+		spellLevel = spellLevelElement.value;
+		console.log("value changed");
+		console.log(spellLevelElement.value)
+	});
+	
+	const spellListSizeElement = document.getElementById("slots");
+	spellListSizeElement.addEventListener("change", function() {
+		totalSpells = spellListSizeElement.value;
+		console.log("value changed");
+		console.log(spellListSizeElement.value)
+	});
+	
+});
+
+
+
+
+
+
+
+
+function populateAvailableSpells () {
+	if (classList === "") {
+		alert("You haven't selected a class spell list!!!");
+	}
+	getSpellSlots();
+	
+	const spells = generateFullList(classList)
+	
+	document.getElementById("availableSpells").innerHTML = spells;
+	
+}
+
+function generateFullList (list) {
+	spellList = testData[list][0];
 	
 	fullList = "";
 	
 	for (const level in spellList) {
 		
 		for (const spell in spellList[level]) {
+			currentSpell = spellList[level][spell];
 			
-			fullList += spellList[level][spell] + "\n";
-			
+			fullList += currentSpell + "\n";
 			
 		}
+		
+		fillSpellArrays(level, spellList[level]);
+		
 	}
 	
-	document.getElementById("availableSpells").innerHTML = fullList;
+	return fullList;
+}
+
+
+
+function populateRandomList () {
+	if (first.length === 0) {
+		alert("Please generate your available spells first!");
+	}
+	
+	const randSpells = generateRandList();
+	document.getElementById("randomOutput").innerHTML = randSpells;
 	
 }
 
 
+
+function generateRandList () {
+	
+	let spellSet = new Set();
+	let randomSpell = "";
+	let randomSelection = "";
+	
+	
+	if (randomType === "trueRand") {
+		
+		for (let k = 0; k < totalSpells; k++) {
+			randomSpellLevel = Math.floor(Math.random() * 9) + 1;
+			
+			switch (randomSpellLevel) {
+				case 1:
+					do {
+						randomSpell = first[Math.floor(Math.random() * first.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							spellSet.add(randomSpell);
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+					
+					break;
+					
+				case 2:
+					do {
+						randomSpell = second[Math.floor(Math.random() * second.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							spellSet.add(randomSpell);
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+					
+					break;
+					
+				case 3:
+					do {
+						randomSpell = third[Math.floor(Math.random() * third.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							spellSet.add(randomSpell);
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+					
+					break;
+					
+				case 4:
+					do {
+						randomSpell = fourth[Math.floor(Math.random() * fourth.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							spellSet.add(randomSpell);
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+					
+					break;
+					
+				case 5:
+					do {
+						randomSpell = fifth[Math.floor(Math.random() * fifth.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							spellSet.add(randomSpell);
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+					
+					break;
+					
+				case 6:
+					do {
+						randomSpell = sixth[Math.floor(Math.random() * sixth.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							spellSet.add(randomSpell);
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+					
+					break;
+					
+				case 7:
+					do {
+						randomSpell = seventh[Math.floor(Math.random() * seventh.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							spellSet.add(randomSpell);
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+					
+					break;
+					
+				case 8:
+					do {
+						randomSpell = eighth[Math.floor(Math.random() * eighth.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							spellSet.add(randomSpell);
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+					
+					break;
+					
+				case 9:
+					do {
+						randomSpell = ninth[Math.floor(Math.random() * ninth.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							spellSet.add(randomSpell);
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+					
+					break;
+			}
+			
+		}
+			
+	}
+	let remainder = 0;
+	let remainderBool = false;
+	let remainderActive = false;
+	
+	if (totalSpells % spellLevel !== 0) {
+		remainder = totalSpells % spellLevel;
+		remainderBool = true;
+		
+	}
+	
+	//for each level of spell
+	for (let i = 1; i <= spellLevel; i++) {
+		let spellsPerLevel = Math.floor(totalSpells / spellLevel);
+		
+		for (let j = 0; j < spellsPerLevel; j++) {
+			let alreadyAdded = false;
+			
+			switch (i) {
+				case 1:
+					if (first.length < spellsPerLevel) {
+						spellsPerLevel = first.length;
+						
+					}
+					
+					do {
+						
+						if (remainderBool && ((j + 1) === spellsPerLevel)) {
+							remainderActive = true;
+							remainderBool = false;
+						
+						}
+					
+						randomSpell = first[Math.floor(Math.random() * first.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							
+							spellSet.add(randomSpell);
+							console.log("added to the set");
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+							
+						}
+						
+						
+						
+					} while (alreadyAdded);
+					
+					break;
+					
+				case 2:
+					if (second.length < spellsPerLevel) {
+						spellsPerLevel = second.length;
+						
+					} 
+					
+					do {
+						
+						if (remainderBool) {
+							remainderActive = true;
+							remainderBool = false;
+						}
+						
+						randomSpell = second[Math.floor(Math.random() * second.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							
+							spellSet.add(randomSpell);
+							console.log("added to the set");
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+				
+					break;
+					
+				case 3:
+					if (third.length < spellsPerLevel) {
+						spellsPerLevel = third.length;
+						
+					}
+					
+					do {
+						
+						if (remainderBool) {
+							remainderActive = true;
+							remainderBool = false;
+						}
+						
+						randomSpell = third[Math.floor(Math.random() * third.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							
+							spellSet.add(randomSpell);
+							console.log("added to the set") 
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+				
+					break;
+					
+				case 4:
+					if (fourth.length < spellsPerLevel) {
+						spellsPerLevel = fourth.length;
+						
+					}
+					
+					do {
+						
+						if (remainderBool) {
+							remainderActive = true;
+							remainderBool = false;
+						}
+						
+						randomSpell = fourth[Math.floor(Math.random() * fourth.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							
+							spellSet.add(randomSpell);
+							console.log("added to the set") 
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+				
+					break;
+					
+				case 5:
+					if (fifth.length < spellsPerLevel) {
+						spellsPerLevel = fifth.length;
+						
+					}
+					
+					do {
+						
+						if (remainderBool) {
+							remainderActive = true;
+							remainderBool = false;
+						}
+						
+						randomSpell = fifth[Math.floor(Math.random() * fifth.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							
+							spellSet.add(randomSpell);
+							console.log("added to the set") 
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+							remainderBool = false;
+						}
+						
+					} while (alreadyAdded);
+					
+				
+					break;
+					
+				case 6:
+					if (sixth.length < spellsPerLevel) {
+						spellsPerLevel = sixth.length;
+						
+					}
+					
+					do {
+						
+						if (remainderBool) {
+							remainderActive = true;
+							remainderBool = false;
+							remainderBool = false;
+						}
+						
+						randomSpell = sixth[Math.floor(Math.random() * sixth.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							
+							spellSet.add(randomSpell);
+							console.log("added to the set") 
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+				
+					break;
+					
+				case 7:
+					if (seventh.length < spellsPerLevel) {
+						spellsPerLevel = seventh.length;
+						
+					}
+					
+					do {
+						
+						if (remainderBool) {
+							remainderActive = true;
+							remainderBool = false;
+						}
+						
+						randomSpell = seventh[Math.floor(Math.random() * seventh.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							
+							spellSet.add(randomSpell);
+							console.log("added to the set") 
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+				
+					break;
+					
+				case 8:
+					if (eighth.length < spellsPerLevel) {
+						spellsPerLevel = eighth.length;
+						
+					}
+					
+					do {
+						
+						if (remainderBool) {
+							remainderActive = true;
+							remainderBool = false;
+						}
+						
+						randomSpell = eighth[Math.floor(Math.random() * eighth.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							
+							spellSet.add(randomSpell);
+							console.log("added to the set") 
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+				
+					break;
+					
+				case 9:
+					if (ninth.length < spellsPerLevel) {
+						spellsPerLevel = ninth.length;
+						
+					}
+					
+					do {
+						
+						if (remainderBool) {
+							remainderActive = true;
+							remainderBool = false;
+						}
+						
+						randomSpell = ninth[Math.floor(Math.random() * ninth.length)];
+						
+						if (!spellSet.has(randomSpell)) {
+							
+							spellSet.add(randomSpell);
+							console.log("added to the set") 
+							alreadyAdded = false;
+							
+						} else {
+							alreadyAdded = true;
+						}
+						
+					} while (alreadyAdded);
+					
+				
+					break;
+					
+					
+			}
+			
+			if (remainder !== 0) {
+				
+				if (remainderActive) {
+					j--;
+					remainder--;
+					remainderActive = false;
+					
+				}
+				
+			}
+			
+		}
+		
+		remainderBool = true;
+		
+	}
+	
+	console.log(spellSet);
+	
+	randomSelection = convertSetToSring(spellSet);
+	
+	return randomSelection
+}
+
+
+// ----- HELPER METHODS ----- //
+
+function fillSpellArrays (level, levelArray) {
+	
+	switch (level) {
+		case "first":
+			first.push(...levelArray);
+			break;
+		
+		case "second":
+			second.push(...levelArray);
+			break;
+		
+		case "third":
+			third.push(...levelArray);
+			break;
+		
+		case "fourth":
+			fourth.push(...levelArray);
+			break;
+		
+		case "fifth":
+			fifth.push(...levelArray);
+			break;
+		
+		case "sixth":
+			sixth.push(...levelArray);
+			break;
+		
+		case "seventh":
+			seventh.push(...levelArray);
+			break;
+		
+		case "eighth":
+			eighth.push(...levelArray);
+			break;
+		
+		case "ninth":
+			ninth.push(...levelArray);
+			break;
+			
+	}
+	
+}
+
+function setSpellList (list) {
+	classList = list;
+	
+	//console.log("class list")
+	//console.log(classList)
+	
+}
+
+function setSpellLevel (selectedLevel) {
+	spellLevel = selectedLevel;
+	
+	//console.log("spell level")
+	//console.log(spellLevel)
+	
+}
+
+function getSpellSlots () {
+	totalSpells = document.getElementById("slots").value;
+	
+	if (totalSpells === "") {
+		alert("You need to specify how many spells you can prepare!!!");
+	}
+	
+	if (totalSpells > 40) {
+			totalSpells = 40;
+	}
+	
+	//console.log("spell slots")
+	//console.log(totalSpells)
+	
+}
+
+function setRandomType (randValue) {
+		
+	randomType = randValue;
+	
+	//console.log("radio button types")
+	//console.log(randomType)
+	
+}
+
+function convertSetToSring(toBeConverted) {
+	let builtString = "";
+	
+	toBeConverted.forEach(value => builtString += value + "\n");
+	
+	return builtString;
+}
